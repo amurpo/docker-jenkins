fc1996e35163ed80a8de82c9fd3af5611de4e696
amurpo <54181842+amurpo@users.noreply.github.com>
2021-03-30 18:12:49 -0300

    Update hello.java
    prueba 2

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This will be printed");
-        System.out.println("Wolas");
+        System.out.println("Wolas esto es una prueba");
     }
 }
